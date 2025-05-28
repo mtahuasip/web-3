6e6ee7014ef50946b55b61e93519e8a815ea4e02
Matt Logan Tahuasi Pariamo <mtahuasip@fcpn.edu.bo>
2025-05-27 23:44:47 -0400

    Refactor código: mejorar formato y estilo en archivos de configuración y URLs de la aplicación 'cursos'

--- segundo-parcial/cursos/urls.py
+++ segundo-parcial/cursos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listado_cursos/', views.listado_cursos,name='listado_cursos'),
-    path('detalle_curso/<int:codigo>/', views.detalle_curso,name='detalle_curso'),
+    path("listado_cursos/", views.listado_cursos, name="listado_cursos"),
+    path("detalle_curso/<int:codigo>/", views.detalle_curso, name="detalle_curso"),
 ]
